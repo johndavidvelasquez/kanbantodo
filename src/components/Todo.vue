4df9550321a0044ea4bf0bef2c623192ead217aa
johndavidvelasquez <johndavidvelaquezz@gmail.com>
2020-10-02 18:36:46 +0800

    Completed template

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -6,7 +6,7 @@
         <v-col cols="6" md="4" ma="6">
           <v-card class="mx-auto" min-width="400">
             <v-card-title id="cardTodoTitle">
-              <v-icon large color="white">mdi-message-text</v-icon>Todo
+              <v-icon color="white">mdi-message-text</v-icon>Todo
             </v-card-title>
             <v-list>
               <draggable class="list-group" :list="todo" group="tasks">
@@ -35,7 +35,6 @@
                 </v-list-item>
               </draggable>
             </v-list>
-
             <v-card-actions>
               <v-dialog v-model="dialog" persistent max-width="600px">
                 <template v-slot:activator="{ on, attrs }">
@@ -83,29 +82,37 @@
       <!-- INPROGRESS -->
       <v-row>
         <v-col cols="6" md="4" ma="6">
-          <v-card class="mx-auto" min-width="500">
-            <v-list-item>
-              <v-list-item-content>
-                <div class="overline">IN PROGRESS</div>
-              </v-list-item-content>
-            </v-list-item>
-            <div>
-              <v-list>
-                <draggable class="list-group" :list="inprogress" group="tasks">
-                  <v-list-item
-                    class="list-group-item"
-                    v-for="(element, index) in inprogress"
-                    :key="index"
-                  >
-                    <v-card>
+          <v-card class="mx-auto" min-width="400">
+            <v-card-title id="cardInProgressTitle">
+              <v-icon color="white">mdi-progress-check</v-icon>IN PROGRESS
+            </v-card-title>
+            <v-list>
+              <draggable class="list-group" :list="inprogress" group="tasks">
+                <v-list-item
+                  class="list-group-item"
+                  v-for="(element, index) in inprogress"
+                  :key="index"
+                >
+                  <v-card class="itemCard">
+                    <v-card-title class="itemCardTitleInProgress">
                       <v-list-item-title
                         v-html="element.name"
                       ></v-list-item-title>
-                    </v-card>
-                  </v-list-item>
-                </draggable>
-              </v-list>
-            </div>
+                    </v-card-title>
+                    <v-card-actions>
+                      <div class="actions-container">
+                        <v-btn class="mx-2" x-small top right fab>
+                          <v-icon dark> mdi-pencil </v-icon>
+                        </v-btn>
+                        <v-btn class="mx-2" x-small top right fab>
+                          <v-icon dark> mdi-delete-empty </v-icon>
+                        </v-btn>
+                      </div>
+                    </v-card-actions>
+                  </v-card>
+                </v-list-item>
+              </draggable>
+            </v-list>
           </v-card>
         </v-col>
       </v-row>
@@ -113,27 +120,37 @@
       <!-- COMPLETED -->
       <v-row>
         <v-col cols="6" md="4" ma="6">
-          <v-card class="mx-auto" min-width="500">
-            <v-list-item>
-              <v-list-item-content>
-                <div class="overline">COMPLETED</div>
-              </v-list-item-content>
-            </v-list-item>
-            <div>
-              <v-list>
-                <draggable class="list-group" :list="completed" group="tasks">
-                  <v-list-item
-                    class="list-group-item"
-                    v-for="(element, index) in completed"
-                    :key="index"
-                  >
-                    <v-list-item-title
-                      v-html="element.name"
-                    ></v-list-item-title>
-                  </v-list-item>
-                </draggable>
-              </v-list>
-            </div>
+          <v-card class="mx-auto" min-width="400">
+            <v-card-title id="cardCompletedTitle">
+              <v-icon color="white">mdi-check</v-icon>COMPLETED
+            </v-card-title>
+            <v-list>
+              <draggable class="list-group" :list="completed" group="tasks">
+                <v-list-item
+                  class="list-group-item"
+                  v-for="(element, index) in completed"
+                  :key="index"
+                >
+                  <v-card class="itemCard">
+                    <v-card-title class="itemCardTitleCompleted">
+                      <v-list-item-title
+                        v-html="element.name"
+                      ></v-list-item-title>
+                    </v-card-title>
+                    <v-card-actions>
+                      <div class="actions-container">
+                        <v-btn class="mx-2" x-small top right fab>
+                          <v-icon dark> mdi-pencil </v-icon>
+                        </v-btn>
+                        <v-btn class="mx-2" x-small top right fab>
+                          <v-icon dark> mdi-delete-empty </v-icon>
+                        </v-btn>
+                      </div>
+                    </v-card-actions>
+                  </v-card>
+                </v-list-item>
+              </draggable>
+            </v-list>
           </v-card>
         </v-col>
       </v-row>
@@ -202,6 +219,20 @@ export default {
 }
 .itemCardTitleTodo {
   border-top: 5px solid #9c27b0;
+}
+#cardInProgressTitle {
+  background-color: #FFC107;
+  color: white;
+}
+.itemCardTitleInProgress {
+  border-top: 5px solid#FFC107;
+}
+#cardCompletedTitle {
+  background-color: #4CAF50;
+  color: white;
+}
+.itemCardTitleCompleted {
+  border-top: 5px solid#4CAF50;
 }
 .actions-container {
   padding-left: 70%;
